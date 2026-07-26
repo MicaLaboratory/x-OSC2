e8324988ab2b39c1d1a15f6baedbac7b0ea6fea9
Ben <benmarples140@gmail.com>
2026-07-26 15:11:45 +0100

    Lots of errors, WIP
    may not compile

--- components/NVS_Helper_Funcs/NVS_Helper_Funcs.c
+++ components/NVS_Helper_Funcs/NVS_Helper_Funcs.c
@@ -4,7 +4,268 @@
 
 #include "esp_log.h"
 
-#include "../../main/global_nvs.h"
+// #include "../../main/global_nvs.h"
+
+#include "NVS_Helper_Funcs.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_err.h"
+
+#define PINCOUNT 28
+
+typedef enum
+{
+    AP = 1,
+    STA,
+    AP_MODE_END
+} AP_Mode;
+
+typedef enum
+{
+    GPIO_OFF,
+    GPIO_ANALOGUE,
+    GPIO_DIGITAL
+} GPIO_State;
+
+typedef enum
+{
+    GPIO_OUTPUT,
+    GPIO_INPUT,
+} GPIO_IO;
+
+typedef struct
+{
+    AP_Mode network_mode;
+    const char *ap_ssid;
+    const char *ap_password;
+    const char *sta_ssid;
+    const char *sta_password;
+} Network_Setttings_t;
+
+typedef struct
+{
+    const char *remote_ip;
+    uint16_t remote_port;
+    const char *local_ip;
+    uint16_t local_port;
+    bool bundle;
+    bool address_prefix;
+} Osc_Settings_t;
+
+typedef struct
+{
+    uint32_t gpio_rate;
+    GPIO_State pin_mode[PINCOUNT];
+    GPIO_IO pin_io[PINCOUNT];
+} GPIO_Settings_t;
+
+typedef struct
+{
+    Network_Setttings_t net_settings;
+    Osc_Settings_t osc_settings;
+    GPIO_Settings_t gpio_settings;
+} NVS_Global;
+
+typedef struct
+{
+    const char *name;
+    size_t offset;
+    size_t size;
+    FieldType type;
+} FieldDesc;
+
+static const FieldDesc g_fields[] = {
+    {"net_settings.network_mode",
+     offsetof(NVS_Global, net_settings.network_mode),
+     sizeof(((NVS_Global *)0)->net_settings.network_mode),
+     FIELD_ENUM},
+
+    {"net_settings.ap_ssid",
+     offsetof(NVS_Global, net_settings.ap_ssid),
+     sizeof(((NVS_Global *)0)->net_settings.ap_ssid),
+     FIELD_STR},
+
+    {"net_settings.ap_password",
+     offsetof(NVS_Global, net_settings.ap_password),
+     sizeof(((NVS_Global *)0)->net_settings.ap_password),
+     FIELD_STR},
+
+    {"net_settings.sta_ssid",
+     offsetof(NVS_Global, net_settings.sta_ssid),
+     sizeof(((NVS_Global *)0)->net_settings.sta_ssid),
+     FIELD_STR},
+
+    {"net_settings.sta_password",
+     offsetof(NVS_Global, net_settings.sta_password),
+     sizeof(((NVS_Global *)0)->net_settings.sta_password),
+     FIELD_STR},
+
+    {"osc_settings.remote_ip",
+     offsetof(NVS_Global, osc_settings.remote_ip),
+     sizeof(((NVS_Global *)0)->osc_settings.remote_ip),
+     FIELD_STR},
+
+    {"osc_settings.remote_port",
+     offsetof(NVS_Global, osc_settings.remote_port),
+     sizeof(((NVS_Global *)0)->osc_settings.remote_port),
+     FIELD_U16},
+
+    {"osc_settings.local_ip",
+     offsetof(NVS_Global, osc_settings.local_ip),
+     sizeof(((NVS_Global *)0)->osc_settings.local_ip),
+     FIELD_STR},
+
+    {"osc_settings.local_port",
+     offsetof(NVS_Global, osc_settings.local_port),
+     sizeof(((NVS_Global *)0)->osc_settings.local_port),
+     FIELD_U16},
+
+    {"osc_settings.bundle",
+     offsetof(NVS_Global, osc_settings.bundle),
+     sizeof(((NVS_Global *)0)->osc_settings.bundle),
+     FIELD_BOOL},
+
+    {"osc_settings.address_prefix",
+     offsetof(NVS_Global, osc_settings.address_prefix),
+     sizeof(((NVS_Global *)0)->osc_settings.address_prefix),
+     FIELD_BOOL},
+
+    {"gpio_settings.gpio_rate",
+     offsetof(NVS_Global, gpio_settings.gpio_rate),
+     sizeof(((NVS_Global *)0)->gpio_settings.gpio_rate),
+     FIELD_U32},
+};
+
+// Functions
+
+esp_err_t nvs_update(NVS_Global *nvs, const char *field_name, const void *value);
+esp_err_t nvs_populate_value(NVS_Global *nvs, const char *field_name, const void *default_value);
+esp_err_t nvs_populate_all(NVS_Global *nvs, const NVS_Global *defaults);
+
+//
+
+static inline const void *get_default_value(const NVS_Global *defaults, const FieldDesc *fd)
+{
+    return (const uint8_t *)defaults + fd->offset;
+}
+
+
+esp_err_t nvs_update(NVS_Global *nvs, const char *field_name, const void *value)
+{
+    for (size_t i = 0; i < sizeof(g_fields) / sizeof(g_fields[0]); i++)
+    {
+        const FieldDesc *fd = &g_fields[i];
+        esp_err_t err;
+
+        if (strcmp(fd->name, field_name) != 0)
+            continue;
+
+        void *target = (uint8_t *)nvs + fd->offset;
+
+        switch (fd->type)
+        {
+        case FIELD_U32:
+            err = nvs_save_value("GLOBAL", field_name, NVS_TYPE_U32, value);
+            *(uint32_t *)target = *(const uint32_t *)value;
+            return err;
+
+        case FIELD_U16:
+            err = nvs_save_value("GLOBAL", field_name, NVS_TYPE_U16, value);
+            *(uint16_t *)target = *(const uint16_t *)value;
+            return err;
+
+        case FIELD_BOOL:
+            err = nvs_save_value("GLOBAL", field_name, NVS_TYPE_U8, value);
+            *(bool *)target = *(const bool *)value;
+            return err;
+
+        case FIELD_ENUM:
+            err = nvs_save_value("GLOBAL", field_name, NVS_TYPE_U32, value);
+            *(uint32_t *)target = *(const uint32_t *)value;
+            return err;
+
+        case FIELD_STR:
+            err = nvs_save_value("GLOBAL", field_name, NVS_TYPE_STR, value);
+            *(const char **)target = (const char *)value;
+            return err;
+        }
+    }
+
+    return ESP_ERR_NOT_FOUND;
+}
+
+esp_err_t nvs_populate_value(NVS_Global *nvs, const char *field_name, const void *default_value)
+{
+    for (size_t i = 0; i < sizeof(g_fields) / sizeof(g_fields[0]); i++)
+    {
+        const FieldDesc *fd = &g_fields[i];
+
+        if (strcmp(fd->name, field_name) != 0)
+            continue;
+
+        void *target = (uint8_t *)nvs + fd->offset;
+        esp_err_t err;
+
+        switch (fd->type)
+        {
+        case FIELD_U32:
+        case FIELD_ENUM:
+        {
+            uint32_t tmp;
+            err = nvs_load_value("GLOBAL", field_name, fd->type, default_value, &tmp);
+            *(uint32_t *)target = (err == ESP_OK) ? tmp : *(const uint32_t *)default_value;
+            return err;
+        }
+
+        case FIELD_U16:
+        {
+            uint16_t tmp;
+            err = nvs_load_value("GLOBAL", field_name, FIELD_U16, default_value, &tmp);
+            *(uint16_t *)target = (err == ESP_OK) ? tmp : *(const uint16_t *)default_value;
+            return err;
+        }
+
+        case FIELD_BOOL:
+        {
+            bool tmp;
+            err = nvs_load_value("GLOBAL", field_name, FIELD_BOOL, default_value, &tmp);
+            *(bool *)target = (err == ESP_OK) ? tmp : *(const bool *)default_value;
+            return err;
+        }
+
+        case FIELD_STR:
+        {
+            char *tmp = NULL;
+            err = nvs_load_value("GLOBAL", field_name, FIELD_STR, default_value, &tmp);
+            *(const char **)target = (err == ESP_OK) ? tmp : (const char *)default_value;
+            return err;
+        }
+
+        default:
+            return ESP_ERR_INVALID_ARG;
+        }
+    }
+
+    return ESP_ERR_NOT_FOUND;
+}
+
+esp_err_t nvs_populate_all(NVS_Global *nvs, const NVS_Global *defaults)
+{
+    for (size_t i = 0; i < sizeof(g_fields) / sizeof(g_fields[0]); i++)
+    {
+        const FieldDesc *fd = &g_fields[i];
+        const void *def = get_default_value(defaults, fd);
+
+        esp_err_t err = nvs_populate_value(nvs, fd->name, def);
+        if (err != ESP_OK) {
+            return err;
+        }
+            
+    }
+
+    return ESP_OK;
+}
 
 // Saving INT
 
