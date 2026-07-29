e5d337a3e04430c84e8e60d6dc6eeb1590dc88c2
Ben <benmarples140@gmail.com>
2026-07-29 15:20:29 +0100

    Remove old definitions for Access Point mode

--- main/main.c
+++ main/main.c
@@ -86,11 +86,6 @@
 
 #define FIRMWARE_VERSION CONFIG_FIRMWARE_VERSION
 
-/* AP Configuration */
-#define EXAMPLE_ESP_WIFI_AP_SSID CONFIG_ESP_WIFI_AP_SSID
-#define EXAMPLE_ESP_WIFI_AP_PASSWD CONFIG_ESP_WIFI_AP_PASSWORD
-
-
 /* The event group allows multiple bits for each event, but we only care about two events:
  * - we are connected to the AP with an IP
  * - we failed to connect after the maximum amount of retries */
